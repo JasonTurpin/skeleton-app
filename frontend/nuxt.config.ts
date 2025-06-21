19172290aa8982abf09670a8d152096b470de8c9
JasonTurpin <jasonaturpin@gmail.com>
2025-06-21 09:50:40 -0400

    Configure API routes

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -1,4 +1,13 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
+    devtools: { enabled: true },
+    modules: ['@nuxtjs/tailwindcss'],
+    runtimeConfig: {
+        apiBase: process.env.API_BASE || 'http://localhost/api',
+
+        public: {
+            // Exposed to browser (client-side)
+            apiBase: process.env.NUXT_PUBLIC_API_BASE || 'http://localhost:8888/api'
+        }
+    }
 })
