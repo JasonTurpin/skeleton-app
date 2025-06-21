707a09263043745abfd46b6d8969c87fb4933039
JasonTurpin <jasonaturpin@gmail.com>
2025-06-21 09:38:30 -0400

    Add a data access layer between controller and recipe report

---
+++ app/Services/Service.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Base Service class
+ * File : app/Services/Service.php
+ *
+ * PHP version 8.2
+ *
+ * @author Jason Turpin <jasonaturpin@gmail.com>
+ * @link   https://github.com/JasonTurpin/skeleton-app/blob/main/app/Services/Service.php
+ */
+namespace App\Services;
+
+/**
+ * Base Service class
+ *
+ * @author Jason Turpin <jasonaturpin@gmail.com>
+ * @link   https://github.com/JasonTurpin/skeleton-app/blob/main/app/Services/Service.php
+ */
+abstract class Service  {
+
+}
