7b82d3f19d75c576df5d54f328b08cecbc94375d
JasonTurpin <jasonaturpin@gmail.com>
2025-06-21 09:33:51 -0400

    Add recipe database seeders

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -12,6 +12,10 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
+        $this->call([
+            AlaskanSeafoodSeeder::class,
+//            LargeRecipeSeeder::class,  // Uncomment to run the large recipe seeder
+        ]);
         // \App\Models\User::factory(10)->create();
 
         // \App\Models\User::factory()->create([
