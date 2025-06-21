c7107255ce3179ba127595c7465542181f34fc54
JasonTurpin <jasonaturpin@gmail.com>
2025-06-21 09:42:45 -0400

    Add recipe api routes

---
+++ app/Http/Controllers/Api/RecipesController.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Fetches recipes
+ * File : app/Http/Controllers/Api/RecipesController.php
+ *
+ * PHP version 8.2
+ *
+ * @author Jason Turpin <jasonaturpin@gmail.com>
+ * @link   https://github.com/JasonTurpin/skeleton-app/blog/main/app/Http/Controllers/Api/RecipesController.php
+ */
+namespace App\Http\Controllers\Api;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Http\Resources\RecipeListResource;
+use App\Services\RecipeFinder\RecipeFinder;
+
+/**
+ * Fetches recipes
+ *
+ * @author Jason Turpin <jasonaturpin@gmail.com>
+ * @link   https://github.com/JasonTurpin/skeleton-app/blog/main/app/Http/Controllers/Api/RecipesController.php
+ */
+class RecipesController extends Controller {
+    /**
+     * Fetches a list of recipes based on search parameters
+     *
+     * @param Request      $request Request object containing search parameters
+     * @param RecipeFinder $finder  The service to find recipes based on search terms
+     * @return \Illuminate\Http\Resources\Json\AnonymousResourceCollection
+     */
+    public function index(Request $request, RecipeFinder $finder) {
+        $searchTerms = $request->only(['email', 'ingredient', 'keyword']);
+        return RecipeListResource::collection($finder->search($searchTerms));
+    }
+}
